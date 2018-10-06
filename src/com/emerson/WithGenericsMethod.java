a561cafd057d9f437e83c4466721a86647f2046d
Emerson Doyah <emerson.doyah@gmail.com>
2018-10-06 11:12:25 -0500

    add additional comments

--- src/com/emerson/WithGenericsMethod.java
+++ src/com/emerson/WithGenericsMethod.java
@@ -1,10 +1,10 @@
 package com.emerson;
 
-public class WithGenericsMethod <T>{
+public class WithGenericsMethod {
 
 
     //A generic method that accepts multiple list types
-    public void printMyList(T[] list) {
+    public <T> void printMyList(T[] list) {
         for (T i : list)
             System.out.printf("%s ", i);
         System.out.println();
